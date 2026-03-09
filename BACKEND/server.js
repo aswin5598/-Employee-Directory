d3aa15fdba5bb52f3d757511e87163584b4f58d6
aswin5598 <sphynx633@gmail.com>
2026-03-09 13:06:20 +0530

    cors_fix

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,37 +3,42 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db1");
 
-// load environment variables
 dotenv.config();
-
-// connect to MongoDB
 connectDB();
 
-// initialize express app
 const app = express();
 
-// middleware
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173"
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true
+  })
 );
+
 app.use(express.json());
 
-// routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/employees", require("./routes/employeeRoutes"));
 
-// test route (optional but useful)
 app.get("/", (req, res) => {
   res.send("API Running Successfully");
 });
 
-// port
 const PORT = process.env.PORT || 5000;
 
-// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
