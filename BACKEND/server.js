4cd8a91a9ddb000068e94938650766e67105ee6c
aswin5598 <sphynx633@gmail.com>
2026-03-09 11:33:47 +0530

    add(2) env

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,7 +13,12 @@ connectDB();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // routes
